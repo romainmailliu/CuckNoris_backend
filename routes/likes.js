9cc8f52be5edf7f0b0f7dc78e1d6e5e8f5c019fc
Thibaut <thibaut.visensang@orange.fr>
2026-02-12 19:22:06 +0100

    backendLikes

--- routes/likes.js
+++ routes/likes.js
@@ -3,4 +3,59 @@ var router = express.Router();
 
 var Like = require("../models/likes");
 
-router.post("/tweet/:tweetId/like", function (req, res) { const newLike = new Like
+// Création d'un like
+// POST /likes
+router.post("/", function (req, res) {
+  const { user_id, tweet_id } = req.body;
+
+  if (!user_id || !tweet_id) {
+    return res.json({
+      result: false,
+      error: "user_id et tweet_id sont requis",
+    });
+  }
+
+  const newLike = new Like({
+    user_id,
+    tweet_id,
+  });
+
+  newLike
+    .save()
+    .then((data) => {
+      res.json({ result: true, like: data });
+    })
+    .catch((err) => {
+      console.log("Erreur POST /likes :", err);
+      res.json({ result: false, error: "like non créé" });
+    });
+});
+
+// Récupérer tous les likes d'un tweet
+// GET /likes/:tweetId
+router.get("/:tweetId", function (req, res) {
+  Like.find({ tweet_id: req.params.tweetId })
+    .populate("user_id", "firstname")
+    .then((data) => {
+      res.json({ result: true, likes: data });
+    })
+    .catch((err) => {
+      console.log("Erreur GET /likes/:tweetId :", err);
+      res.json({ result: false, error: "likes non trouvés" });
+    });
+});
+
+// Suppression d'un like
+// DELETE /likes/:id
+router.delete("/:id", function (req, res) {
+  Like.deleteOne({ _id: req.params.id })
+    .then(() => {
+      res.json({ result: true });
+    })
+    .catch((err) => {
+      console.log("Erreur DELETE /likes/:id :", err);
+      res.json({ result: false, error: "like non supprimé" });
+    });
+});
+
+module.exports = router;
